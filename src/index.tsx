fe6ec9c5837e2d3dd0e71a085e45ea1761230fbd
vinodShelar <vinodshelar7757@gmail.com>
2025-05-13 20:48:33 +0400

    initalize repo

---
+++ src/index.tsx
@@ -0,0 +1,464 @@
+type supportedTimeZones = 
+  | "Africa/Abidjan"
+  | "Africa/Accra"
+  | "Africa/Addis_Ababa"
+  | "Africa/Algiers"
+  | "Africa/Asmara"
+  | "Africa/Bamako"
+  | "Africa/Bangui"
+  | "Africa/Banjul"
+  | "Africa/Bissau"
+  | "Africa/Blantyre"
+  | "Africa/Brazzaville"
+  | "Africa/Bujumbura"
+  | "Africa/Cairo"
+  | "Africa/Casablanca"
+  | "Africa/Ceuta"
+  | "Africa/Conakry"
+  | "Africa/Dakar"
+  | "Africa/Dar_es_Salaam"
+  | "Africa/Djibouti"
+  | "Africa/Johannesburg"
+  | "Africa/Juba"
+  | "Africa/Kampala"
+  | "Africa/Khartoum"
+  | "Africa/Kigali"
+  | "Africa/Kinshasa"
+  | "Africa/Lagos"
+  | "Africa/Libreville"
+  | "Africa/Lome"
+  | "Africa/Luanda"
+  | "Africa/Lubumbashi"
+  | "Africa/Lusaka"
+  | "Africa/Malabo"
+  | "Africa/Maputo"
+  | "Africa/Maseru"
+  | "Africa/Mbabane"
+  | "Africa/Mogadishu"
+  | "Africa/Monrovia"
+  | "Africa/Nairobi"
+  | "Africa/Ndjamena"
+  | "Africa/Niamey"
+  | "Africa/Nouakchott"
+  | "Africa/Ouagadougou"
+  | "Africa/Porto-Novo"
+  | "Africa/Sao_Tome"
+  | "Africa/Tripoli"
+  | "Africa/Tunis"
+  | "Africa/Windhoek"
+  | "America/Adak"
+  | "America/Anchorage"
+  | "America/Anguilla"
+  | "America/Antigua"
+  | "America/Araguaina"
+  | "America/Argentina/Buenos_Aires"
+  | "America/Argentina/Catamarca"
+  | "America/Argentina/ComodRivadavia"
+  | "America/Argentina/Cordoba"
+  | "America/Argentina/Jujuy"
+  | "America/Argentina/La_Rioja"
+  | "America/Argentina/Mendoza"
+  | "America/Argentina/Rio_Gallegos"
+  | "America/Argentina/Salta"
+  | "America/Argentina/San_Juan"
+  | "America/Argentina/San_Luis"
+  | "America/Argentina/Tucuman"
+  | "America/Argentina/Ushuaia"
+  | "America/Aruba"
+  | "America/Asuncion"
+  | "America/Atikokan"
+  | "America/Atka"
+  | "America/Bahia"
+  | "America/Bahia_Banderas"
+  | "America/Barbados"
+  | "America/Belem"
+  | "America/Belize"
+  | "America/Blanc-Sablon"
+  | "America/Boa_Vista"
+  | "America/Bogota"
+  | "America/Boise"
+  | "America/Cambridge_Bay"
+  | "America/Campo_Grande"
+  | "America/Cancun"
+  | "America/Caracas"
+  | "America/Cayenne"
+  | "America/Cayman"
+  | "America/Chicago"
+  | "America/Chihuahua"
+  | "America/Costa_Rica"
+  | "America/Creston"
+  | "America/Cuiaba"
+  | "America/Curacao"
+  | "America/Danmarkshavn"
+  | "America/Dawson"
+  | "America/Dawson_Creek"
+  | "America/Denver"
+  | "America/Detroit"
+  | "America/Dominica"
+  | "America/Edmonton"
+  | "America/Eirunepe"
+  | "America/El_Salvador"
+  | "America/Fortaleza"
+  | "America/Glace_Bay"
+  | "America/Godthab"
+  | "America/Goose_Bay"
+  | "America/Grand_Turk"
+  | "America/Grenada"
+  | "America/Guadeloupe"
+  | "America/Guayaquil"
+  | "America/Guyana"
+  | "America/Houston"
+  | "America/Indiana/Indianapolis"
+  | "America/Indiana/Knox"
+  | "America/Indiana/Marengo"
+  | "America/Indiana/Petersburg"
+  | "America/Indiana/Tell_City"
+  | "America/Indiana/Vincennes"
+  | "America/Indiana/Winamac"
+  | "America/Inuvik"
+  | "America/Iqaluit"
+  | "America/Jamaica"
+  | "America/Juneau"
+  | "America/Kentucky/Louisville"
+  | "America/Kentucky/Monticello"
+  | "America/Kralendijk"
+  | "America/La_Paz"
+  | "America/Lima"
+  | "America/Los_Angeles"
+  | "America/Louisville"
+  | "America/Maceio"
+  | "America/Managua"
+  | "America/Manaus"
+  | "America/Marigot"
+  | "America/Martinique"
+  | "America/Matamoros"
+  | "America/Mexico_City"
+  | "America/Miquelon"
+  | "America/Moncton"
+  | "America/Monterrey"
+  | "America/Montevideo"
+  | "America/Montreal"
+  | "America/Moroni"
+  | "America/Nassau"
+  | "America/New_York"
+  | "America/Nipigon"
+  | "America/Nome"
+  | "America/Noronha"
+  | "America/North_Dakota/Beulah"
+  | "America/North_Dakota/Center"
+  | "America/North_Dakota/New_Salem"
+  | "America/Ojinaga"
+  | "America/Panama"
+  | "America/Pangnirtung"
+  | "America/Paramaribo"
+  | "America/Phoenix"
+  | "America/Port_of_Spain"
+  | "America/Port-au-Prince"
+  | "America/Providence"
+  | "America/Puerto_Rico"
+  | "America/Punta_Arenas"
+  | "America/Rainy_River"
+  | "America/Rankin_Inlet"
+  | "America/Recife"
+  | "America/Regina"
+  | "America/Resolute"
+  | "America/Rio_Branco"
+  | "America/Santa_Isabel"
+  | "America/Santarem"
+  | "America/Santiago"
+  | "America/Santo_Domingo"
+  | "America/Sao_Paulo"
+  | "America/Scoresbysund"
+  | "America/Sitka"
+  | "America/St_Johns"
+  | "America/Swift_Current"
+  | "America/Tegucigalpa"
+  | "America/Thule"
+  | "America/Thunder_Bay"
+  | "America/Tijuana"
+  | "America/Toronto"
+  | "America/Tortola"
+  | "America/Vancouver"
+  | "America/Whitehorse"
+  | "America/Winnipeg"
+  | "America/Yakutat"
+  | "America/Yellowknife"
+  | "Antarctica/Palmer"
+  | "Antarctica/Rothera"
+  | "Asia/Almaty"
+  | "Asia/Amman"
+  | "Asia/Anadyr"
+  | "Asia/Aqtau"
+  | "Asia/Aqtobe"
+  | "Asia/Baghdad"
+  | "Asia/Baku"
+  | "Asia/Bangkok"
+  | "Asia/Barnaul"
+  | "Asia/Beirut"
+  | "Asia/Bishkek"
+  | "Asia/Brunei"
+  | "Asia/Calcutta"
+  | "Asia/Chita"
+  | "Asia/Choibalsan"
+  | "Asia/Colombo"
+  | "Asia/Damascus"
+  | "Asia/Dhaka"
+  | "Asia/Dili"
+  | "Asia/Dubai"
+  | "Asia/Dushanbe"
+  | "Asia/Gaza"
+  | "Asia/Hong_Kong"
+  | "Asia/Hovd"
+  | "Asia/Irkutsk"
+  | "Asia/Jakarta"
+  | "Asia/Jayapura"
+  | "Asia/Kabul"
+  | "Asia/Kamchatka"
+  | "Asia/Karachi"
+  | "Asia/Kathmandu"
+  | "Asia/Kolkata"
+  | "Asia/Kuala_Lumpur"
+  | "Asia/Kuwait"
+  | "Asia/Macau"
+  | "Asia/Magadan"
+  | "Asia/Manila"
+  | "Asia/Muscat"
+  | "Asia/Nicosia"
+  | "Asia/Novokuznetsk"
+  | "Asia/Novosibirsk"
+  | "Asia/Omsk"
+  | "Asia/Oral"
+  | "Asia/Phnom_Penh"
+  | "Asia/Pontianak"
+  | "Asia/Pyongyang"
+  | "Asia/Qatar"
+  | "Asia/Qyzylorda"
+  | "Asia/Rangoon"
+  | "Asia/Riyadh"
+  | "Asia/Sakhalin"
+  | "Asia/Samarkand"
+  | "Asia/Seoul"
+  | "Asia/Shanghai"
+  | "Asia/Singapore"
+  | "Asia/Taipei"
+  | "Asia/Tashkent"
+  | "Asia/Tbilisi"
+  | "Asia/Tehran"
+  | "Asia/Thimphu"
+  | "Asia/Tokyo"
+  | "Asia/Ulaanbaatar"
+  | "Asia/Urumqi"
+  | "Asia/Vientiane"
+  | "Asia/Vladivostok"
+  | "Asia/Yakutsk"
+  | "Asia/Yangon"
+  | "Asia/Yekaterinburg"
+  | "Australia/Adelaide"
+  | "Australia/Brisbane"
+  | "Australia/Broken_Hill"
+  | "Australia/Currie"
+  | "Australia/Darwin"
+  | "Australia/Hobart"
+  | "Australia/Lindeman"
+  | "Australia/Lord_Howe"
+  | "Australia/Melbourne"
+  | "Australia/Perth"
+  | "Australia/Sydney"
+  | "Europe/Amsterdam"
+  | "Europe/Andorra"
+  | "Europe/Athens"
+  | "Europe/Belgrade"
+  | "Europe/Berlin"
+  | "Europe/Bratislava"
+  | "Europe/Brussels"
+  | "Europe/Bucharest"
+  | "Europe/Budapest"
+  | "Europe/Chisinau"
+  | "Europe/Copenhagen"
+  | "Europe/Dublin"
+  | "Europe/Gibraltar"
+  | "Europe/Guernsey"
+  | "Europe/Helsinki"
+  | "Europe/Isle_of_Man"
+  | "Europe/Istanbul"
+  | "Europe/Jersey"
+  | "Europe/Kaliningrad"
+  | "Europe/Kiev"
+  | "Europe/Lisbon"
+  | "Europe/Ljubljana"
+  | "Europe/London"
+  | "Europe/Luxembourg"
+  | "Europe/Madrid"
+  | "Europe/Malta"
+  | "Europe/Mariehamn"
+  | "Europe/Minsk"
+  | "Europe/Monaco"
+  | "Europe/Moscow"
+  | "Europe/Oslo"
+  | "Europe/Paris"
+  | "Europe/Podgorica"
+  | "Europe/Prague"
+  | "Europe/Riga"
+  | "Europe/Rome"
+  | "Europe/Samara"
+  | "Europe/San_Marino"
+  | "Europe/Saratov"
+  | "Europe/Simferopol"
+  | "Europe/Sofia"
+  | "Europe/Stockholm"
+  | "Europe/Tallinn"
+  | "Europe/Tirane"
+  | "Europe/Ulyanovsk"
+  | "Europe/Vaduz"
+  | "Europe/Vatican"
+  | "Europe/Vienna"
+  | "Europe/Vilnius"
+  | "Europe/Volgograd"
+  | "Europe/Warsaw"
+  | "Europe/Zagreb"
+  | "Europe/Zaporozhye"
+  | "Europe/Zurich"
+  | "Indian/Antananarivo"
+  | "Indian/Chagos"
+  | "Indian/Christmas"
+  | "Indian/Cocos"
+  | "Indian/Comoro"
+  | "Indian/Kerguelen"
+  | "Indian/Mahe"
+  | "Indian/Maldives"
+  | "Indian/Reunion"
+  | "Pacific/Apia"
+  | "Pacific/Auckland"
+  | "Pacific/Chatham"
+  | "Pacific/Easter"
+  | "Pacific/Efate"
+  | "Pacific/Enderbury"
+  | "Pacific/Fakaofo"
+  | "Pacific/Fiji"
+  | "Pacific/Funafuti"
+  | "Pacific/Galapagos"
+  | "Pacific/Gambier"
+  | "Pacific/Guadalcanal"
+  | "Pacific/Honolulu"
+  | "Pacific/Johnston"
+  | "Pacific/Kiritimati"
+  | "Pacific/Kosrae"
+  | "Pacific/Marquesas"
+  | "Pacific/Midway"
+  | "Pacific/Nauru"
+  | "Pacific/Niue"
+  | "Pacific/Norfolk"
+  | "Pacific/Noumea"
+  | "Pacific/Pago_Pago"
+  | "Pacific/Palau"
+  | "Pacific/Pitcairn"
+  | "Pacific/Port_Moresby"
+  | "Pacific/Rarotonga"
+  | "Pacific/Tahiti"
+  | "Pacific/Tarawa"
+  | "Pacific/Tongatapu"
+  | "Pacific/Wake"
+  | "Pacific/Wallis";
+  
+
+  export type DateFormats =
+  | 'dd-mm-yyyy'
+  | 'mm-dd-yyyy'
+  | 'yyyy-mm-dd'
+  | 'dd Month yyyy'
+  | 'dd Mon yyyy'
+  | 'dd Month, yyyy'
+  | 'dd Mon, yyyy'
+  | 'HH:mm dd/mm/yyyy'
+  | 'HH:mm dd-mm-yyyy'
+  | 'HH:mm , dd MMM yyyy'
+  | 'HH:mm , dd MMM, yyyy'
+  | 'dd MMM yyyy, HH:mm'
+  | 'HH:mm'
+  | 'hh:mm A'
+  | 'HH:mm:ss'
+  | 'dddd, dd MMM yyyy'
+  | 'relative'
+  | string;
+
+
+interface TimeFormatterOptions {
+  date: Date | string | number;
+  format?: DateFormats;
+  getTimezone?: supportedTimeZones;
+}
+
+
+export function formatTime({ date, format = 'dd-mm-yyyy', getTimezone }: TimeFormatterOptions): string {
+  const getZonedDate = (date: Date): Date => {
+    if (!getTimezone) return date;
+    const formatter = new Intl.DateTimeFormat('en-US', {
+      timeZone: getTimezone,
+      hour12: false,
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    });
+    const parts = formatter.formatToParts(date).reduce((acc, part) => {
+      if (part.type !== 'literal') acc[part.type] = part.value;
+      return acc;
+    }, {} as Record<string, string>);
+    return new Date(`${parts.year}-${parts.month}-${parts.day}T${parts.hour}:${parts.minute}:${parts.second}`);
+  };
+
+  const parseDate = (input: Date | string | number): Date => {
+    if (typeof input === 'number' || !isNaN(Number(input))) return new Date(Number(input));
+    return new Date(input);
+  };
+
+  const dateObj = getZonedDate(parseDate(date));
+  const day = dateObj.getDate().toString().padStart(2, '0');
+  const month = (dateObj.getMonth() + 1).toString().padStart(2, '0');
+  const year = dateObj.getFullYear();
+  const hours = dateObj.getHours();
+  const minutes = dateObj.getMinutes().toString().padStart(2, '0');
+  const seconds = dateObj.getSeconds().toString().padStart(2, '0');
+  const dayName = dateObj.toLocaleDateString('en-US', { weekday: 'long' });
+  const monthNameShort = dateObj.toLocaleString('en-US', { month: 'short' });
+  const monthNameLong = dateObj.toLocaleString('en-US', { month: 'long' });
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  const hours12 = (hours % 12 || 12).toString().padStart(2, '0');
+  const hours24 = hours.toString().padStart(2, '0');
+
+  const now = new Date();
+  const timeDifference = +now - +dateObj;
+  const secondsAgo = Math.floor(timeDifference / 1000);
+  const minutesAgo = Math.floor(secondsAgo / 60);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+
+  switch (format) {
+    case 'dd-mm-yyyy': return `${day}-${month}-${year}`;
+    case 'mm-dd-yyyy': return `${month}-${day}-${year}`;
+    case 'yyyy-mm-dd': return `${year}-${month}-${day}`;
+    case 'dd Month yyyy': return `${day} ${monthNameLong} ${year}`;
+    case 'dd Mon yyyy': return `${day} ${monthNameShort} ${year}`;
+    case 'dd Month, yyyy': return `${day} ${monthNameLong}, ${year}`;
+    case 'dd Mon, yyyy': return `${day} ${monthNameShort}, ${year}`;
+    case 'HH:mm dd/mm/yyyy': return `${hours24}:${minutes} ${day}/${month}/${year}`;
+    case 'HH:mm dd-mm-yyyy': return `${hours24}:${minutes} ${day}-${month}-${year}`;
+    case 'HH:mm , dd MMM yyyy': return `${hours24}:${minutes} , ${day} ${monthNameShort} ${year}`;
+    case 'HH:mm , dd MMM, yyyy': return `${hours24}:${minutes} , ${day} ${monthNameShort}, ${year}`;
+    case 'dd MMM yyyy, HH:mm': return `${day} ${monthNameShort} ${year}, ${hours24}:${minutes}`;
+    case 'HH:mm': return `${hours24}:${minutes}`;
+    case 'hh:mm A': return `${hours12}:${minutes} ${ampm}`;
+    case 'HH:mm:ss': return `${hours24}:${minutes}:${seconds}`;
+    case 'dddd, dd MMM yyyy': return `${dayName}, ${day} ${monthNameShort} ${year}`;
+    case 'relative': {
+      if (secondsAgo < 60) return `${secondsAgo} sec ago`;
+      if (minutesAgo < 60) return `${minutesAgo} min ago`;
+      if (hoursAgo < 24) return `${hoursAgo} hrs ago`;
+      if (daysAgo < 30) return `${daysAgo} days ago`;
+      return `${day} ${monthNameShort} ${year}`;
+    }
+    default: return `${day}-${month}-${year}`;
+  }
+}
